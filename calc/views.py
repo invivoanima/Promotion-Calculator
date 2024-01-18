47c7c4203cab2dfe9a52330632469aaa6250ee76
invitroorbis <holocaust44@naver.com>
2024-01-18 17:33:54 +0900

    -로그인 기능 작업

--- calc/views.py
+++ calc/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
 
 def welcome(request) :
     return render(request, 'calc/index.html')
@@ -7,5 +9,19 @@ def main(request) :
     return render(request, 'calc/main.html')
 
 def login(request) :
-    return render(request, 'calc/login.html')
+    if request.method == "POST":
+        username = request.POST["username"]
+        password1 = request.POST["password1"]
+
+        user = auth.authenticate(
+            request, username=username, password=password1
+        )
+
+        if user is not None :
+            auth.login(request, user)
+            return redirect("calc:main")
+    else :
+        return render(request, 'calc/login.html')
+    
+    
 
